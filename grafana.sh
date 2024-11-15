30a3644b8ab24e094abc0400aefbf3b259a31b09
pashaimran <80830406+pashaimran@users.noreply.github.com>
2024-11-15 17:37:21 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+NAMESPACE="grafana"  # Replace with your namespace
 
+# Get the service account token
+SA_TOKEN=$(oc create token grafana-sa -n $NAMESPACE)
+
+# Create secret for the bearer token
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana-config
+  namespace: $NAMESPACE
+type: Opaque
+stringData:
+  SA_TOKEN: "$SA_TOKEN"
+EOF
